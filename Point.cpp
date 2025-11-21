a5f0d0d36c958cf02bc71af8b41b87aea11f6925
Normand Victor <normand.victor064@gmail.com>
2025-11-21 11:58:17 +0100

    fichier Point finis

---
+++ Point.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Point.h"
+
+namespace points{
+
+double Point::get_x()const {};
+double Point::get_y()const {};
+double Point::get_z()const {};
+void Point::set_x(double x){};
+void Point::set_y(double y){};
+void Point::set_z(double z){};
+void Point::afficher()const{};
+}
---
+++ Point.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace points{
+
+class Point{
+    virtual double get_x() const = 0;
+    virtual double get_y() const = 0;
+    virtual double get_z() const = 0;
+    virtual void set_x(double) = 0;
+    virtual void set_y(double) = 0;
+    virtual void set_z(double) = 0;
+    virtual void afficher() const = 0;
+};
+}
